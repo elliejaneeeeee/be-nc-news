edd1fe13ccc84625d7c638968e1508a190515cd6
Ellie <admin@Ellies-Macbook.local>
2024-04-15 15:22:06 +0100

    Implement get article by id functionality

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const endpoints = require('./endpoints.json')
 
-const { getTopics } = require('./controller/get.controller')
+const { getTopics, getArticleById } = require('./controller/get.controller')
 
 app.get('/api/topics', getTopics)
 
@@ -10,12 +10,26 @@ app.get('/api', (req, res) => {
     res.status(200).send({endpoints})
 })
 
+app.get('/api/articles/:article_id', getArticleById)
+
+
 
 //Invalid route handling
 app.all('*', (req, res) => {
     res.status(404).send({msg: '404 Error: Path not found'})
 })
 
+//Custom error handling
+app.use((err, req, res, next) => {
+    if (err.status && err.msg) {
+        res.status(404).send({msg: err.msg})
+    } else if (err.code === '22P02'){
+        res.status(400).send({msg: '400 Bad Request: Invalid Id'})
+    }
+    else {
+        next()
+    }
+})
 
 //Internal Server Error
 app.use((err, req, res, next) => {
--- models/get.model.js
+++ models/get.model.js
@@ -6,3 +6,10 @@ exports.fetchTopics = () => {
         return rows
     })
 }
+
+exports.fetchArticleById = (article_id) => {
+    return db.query(`SELECT * FROM articles WHERE article_id=$1`, [article_id])
+    .then(({rows}) => {
+        return rows.length === 0 ? Promise.reject({status:400, msg: "404 Error: Resource Doesn't exist"}) : rows
+    })
+}
