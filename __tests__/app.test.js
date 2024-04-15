1704436ac190234890d875ead22436a976b159ee
Ellie <admin@Ellies-Macbook.local>
2024-04-15 11:51:52 +0100

    Add relevant testing for get endpoint

---
+++ __tests__/app.test.js
@@ -0,0 +1,38 @@
+const request = require('supertest')
+const app = require('../app')
+const db = require('../db/connection')
+const seed = require('../db/seeds/seed')
+const { articleData, commentData, topicData, userData} = require('../db/data/test-data/index')
+
+beforeEach(() => {
+    return seed({ topicData, userData, articleData, commentData })
+})
+
+afterAll(() => {
+    db.end()
+})
+
+describe('/api/topics', () => {
+    describe('GET', () => {
+        test('GET 200: responds with an array of all topics in the correct format', () => {
+            return request(app)
+            .get('/api/topics')
+            .expect(200)
+            .then(({body}) => {
+                const topics = body
+                topics.forEach((topic) => {
+                    expect(typeof topic.slug).toBe('string')
+                    expect(typeof topic.description).toBe('string')
+                })
+            })
+        })
+        test('GET 404: responds with a 404 error when path is not found', () => {
+            return request(app)
+            .get('/api/topic')
+            .expect(404)
+            .then(({body}) => {
+                expect(body.msg).toBe('404 Error: Path not found')
+            })
+        })
+    })
+})
