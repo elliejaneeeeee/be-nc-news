7e11b41354ac4709891f46b74606853928dfe449
Ellie <admin@Ellies-Macbook.local>
2024-04-16 11:21:23 +0100

    Add endpoint for get all articles and tweaked error handling

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require('express')
 const app = express()
 const endpoints = require('./endpoints.json')
 
-const { getTopics, getArticleById } = require('./controller/get.controller')
+const { getTopics, getArticles, getArticleById } = require('./controller/get.controller')
 
 app.get('/api/topics', getTopics)
 
 app.get('/api', (req, res) => {
     res.status(200).send({endpoints})
 })
+
+app.get('/api/articles', getArticles)
 
 app.get('/api/articles/:article_id', getArticleById)
 
@@ -22,12 +24,18 @@ app.all('*', (req, res) => {
 //Custom error handling
 app.use((err, req, res, next) => {
     if (err.status && err.msg) {
-        res.status(404).send({msg: err.msg})
-    } else if (err.code === '22P02'){
-        res.status(400).send({msg: '400 Bad Request: Invalid Id'})
+        res.status(err.status).send({msg: err.msg})
+    } else {
+        next(err)
     }
-    else {
-        next()
+})
+
+//PSQL error handling
+app.use((err, req, res, next) => {
+    if (err.code === '22P02') {
+        res.status(400).send({msg: '400 Bad Request: Invalid Id'})
+    } else {
+        next(err)
     }
 })
 
