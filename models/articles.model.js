a67d9ab3a35ed2b2aaf448300c16818fd95e0947
Ellie <admin@Ellies-Macbook.local>
2024-04-17 16:25:49 +0100

    Tweak functionality to include comment count in res

--- models/articles.model.js
+++ models/articles.model.js
@@ -29,7 +29,16 @@ exports.fetchArticlesWithCommentCount = (topic) => {
 }
 
 exports.fetchArticleById = (article_id) => {
-    return db.query(`SELECT * FROM articles WHERE article_id=$1`, [article_id])
+    const SQLStr = format(`
+    SELECT articles.*, 
+    CAST(COUNT(comment_id) AS INT) AS comment_count
+    FROM articles 
+    LEFT JOIN comments ON comments.article_id = articles.article_id 
+    WHERE articles.article_id = $1 
+    GROUP BY articles.article_id;
+    `)
+
+    return db.query(SQLStr, [article_id])
     .then(({rows}) => {
         return rows.length === 0 ? Promise.reject({status:404, msg: "404 Error: Resource Doesn't exist"}) : rows
     })
