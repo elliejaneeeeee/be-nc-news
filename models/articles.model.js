cbb217acb1d0ccf8f67244ca713371187a0edd27
Ellie <admin@Ellies-Macbook.local>
2024-04-19 11:08:10 +0100

    revise formatting

--- models/articles.model.js
+++ models/articles.model.js
@@ -7,34 +7,34 @@ exports.fetchArticlesWithCommentCount = (topic, order = 'desc', sort_by = 'creat
     'desc', 'asc', 'DESC', 'ASC', 
     'created_at', 'title', 'author', 'body', 'created_at', 'topic', 'votes']
 
-    let SQLStr = `
+    let sqlStr = `
     SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url, 
     CAST(COUNT(comment_id) AS INT) AS comment_count
     FROM articles 
     LEFT JOIN comments ON comments.article_id = articles.article_id 
     `
     if (topic && validQueries.includes(topic)) {
-        SQLStr += `WHERE articles.topic = '${topic}' `
+        sqlStr += `WHERE articles.topic = '${topic}' `
     } else if (!topic) {
-        SQLStr += `WHERE articles.topic IS NOT NULL`
+        sqlStr += `WHERE articles.topic IS NOT NULL`
     } else if (!validQueries.includes(sort_by) || !validQueries.includes(order)) {
         return Promise.reject({status: 404, msg: "404 Error: Resource doesn't exist"})
     } else {
         return Promise.reject({status: 404, msg: "404 Error: Resource doesn't exist"})
     }
 
-    SQLStr += `
+    sqlStr += `
     GROUP BY articles.article_id 
     ORDER BY ${sort_by} ${order};`
 
-    return db.query(SQLStr)
+    return db.query(sqlStr)
     .then(({rows}) => {
         return rows.length === 0 ? [] : rows
     })
 }
 
 exports.fetchArticleById = (article_id) => {
-    const SQLStr = format(`
+    const sqlStr = format(`
     SELECT articles.*, 
     CAST(COUNT(comment_id) AS INT) AS comment_count
     FROM articles 
@@ -43,7 +43,7 @@ exports.fetchArticleById = (article_id) => {
     GROUP BY articles.article_id;
     `)
 
-    return db.query(SQLStr, [article_id])
+    return db.query(sqlStr, [article_id])
     .then(({rows}) => {
         return rows.length === 0 ? Promise.reject({status:404, msg: "404 Error: Resource Doesn't exist"}) : rows
     })
@@ -51,14 +51,14 @@ exports.fetchArticleById = (article_id) => {
 
 exports.updateArticleVotes = (article_id, votes) => {
     const values = [votes, article_id]
-    const SQLString = format(
+    const sqlString = format(
         `UPDATE articles 
         SET votes = votes + $1 
         WHERE article_id = $2 
         RETURNING *;`
     )
 
-    return db.query(SQLString, values)
+    return db.query(sqlString, values)
     .then(({rows}) => {
         if (rows.length === 0) {
             return Promise.reject({status: 404, msg: "404 Error: Resource doesn't exist"})
