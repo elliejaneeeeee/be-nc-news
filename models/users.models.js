499ed291a3554d4a8ceb7bcba64b52278d5a8643
Ellie <admin@Ellies-Macbook.local>
2024-04-19 11:08:41 +0100

    Add functionality for fetchUserByUsername

--- models/users.models.js
+++ models/users.models.js
@@ -1,4 +1,5 @@
 const db = require('../db/connection')
+const format = require('pg-format')
 
 exports.fetchUsers = () => {
     return db.query(`
@@ -6,5 +7,18 @@ exports.fetchUsers = () => {
     FROM users;`)
     .then(({rows}) => {
         return rows
+    })
+}
+
+exports.fetchUserByUsername = (username) => {
+    const sqlStr = format(`
+    SELECT * FROM %I 
+    WHERE username = $1;`, 
+    'users')
+
+    return db.query(sqlStr, [username])
+    .then(({rows}) => {
+        const [user] = rows
+        return rows.length === 0 ? Promise.reject({status: 404, msg: "404 Error: Resource doesn't exist"}) : user
     })
 }
