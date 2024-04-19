9148616ee1257837bbb5822b997bc5ff69935d18
Ellie <admin@Ellies-Macbook.local>
2024-04-19 11:10:06 +0100

    Add route for get /:username

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,6 +1,7 @@
 const usersRouter = require('express').Router()
-const { getAllUsers } = require('../controller/users.controller')
+const { getAllUsers, getUserByUsername } = require('../controller/users.controller')
 
 usersRouter.get('/', getAllUsers)
+usersRouter.get('/:username', getUserByUsername)
 
 module.exports = usersRouter
